6f66cceef81d6bd2e1c255f11e60bd366172a1da
Victor Lopez <vic.m.lopez114@gmail.com>
2024-06-13 17:49:40 -0400

    added variables to be used to the form

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-// user input variables
-let entertainer = document.querySelector(".input-box--entertainer");
-let numTickets = document.querySelector(".input-box--tickets");
-let ticketType = document.querySelector(".input-box--type");
+// DOM connections
+let carsFound = document.querySelector(".caras_found");
+let findBtn = document.getElementById("find--btn");
 
-// submit button
-let submitBtn = document.getElementById("btn");
 
-// receipt text
-let receiptEntertainer = document.querySelector(".receipt_text--entertainer");
-let receiptNumTickets = document.querySelector(".receipt_text--tickets");
-let receiptTicketType = document.querySelector(".receipt_text--type");
-let receiptTicketPrice = document.querySelector(".receipt_text--price");
-let receiptTicketTotal = document.querySelector(".receipt_text--total");
-
-submitBtn.onclick = function() {
-	receiptEntertainer.innerText = `Entertainer: ${entertainer.value}`;
-
-	receiptNumTickets.innerText = `# of tickets: ${numTickets.value}`; 
-}
-
+// user input
+let carMaker = document.getElementById("car_maker");
+let carModel = document.getElementById("car_model");

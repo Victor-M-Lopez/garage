d67777965a1bee111ef0ea1cf397efb32aff0b44
Victor Lopez <vic.m.lopez114@gmail.com>
2024-06-13 18:04:46 -0400

    added arrays

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ let findBtn = document.getElementById("find--btn");
 // user input
 let carMaker = document.getElementById("car_maker");
 let carModel = document.getElementById("car_model");
+
+// cars found
+
+let Civic = ["Honda Civic 2009", "Honda Civic 2012", "Honda Civic 2018", "Honda Civic 2021"]
+let CRV = ["Honda CR-V 2009", "Honda CR-V 2011", "Honda CR-V 2019", "Honda CR-V 2022"]
+let 

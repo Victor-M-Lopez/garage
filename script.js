ea4633ff3b9beeff561b8f1d20c9b5a211c564c5
Victor Lopez <vic.m.lopez114@gmail.com>
2024-06-15 22:43:05 -0400

    deleted form from page

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-// DOM connections
-let carsFound = document.querySelector(".caras_found");
-let findBtn = document.getElementById("find--btn");
 
-
-// user input
-let carMaker = document.getElementById("car_maker");
-let carModel = document.getElementById("car_model");
-
-// cars found
-
-let hondaSedan = ["Honda Civic", "Honda Accord", "Honda Clarity"];
-let hondaSUV = ["Honda Passport", "Honda Ridgeline", "Honda Pilot"];
-
-let toyotaSedan = ["Toyota Camery", "Toyota Corolla", "Toyota Supra"];
-let toyotaSUV = ["Toyota RAV4", "Toyota C-HR", "Toyota Highlander"];
-let toyotaTruck = ["Toyota Tundra", "Toyota Tacoma"];

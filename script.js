df712193df3d999c09be4bf508d276b7b69e92a6
Victor Lopez <vic.m.lopez114@gmail.com>
2024-07-13 17:05:06 -0400

    added sign in/sign up modal

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-let loginLi = document.getElementById('login_li');
+let loginModal = document.getElementById('login_modal');
+let myGarageBtn = document.getElementById("login_li");
+let exitBtn = document.getElementById('exit_modal');
+
+myGarageBtn.addEventListener('click', function() {
+  loginModal.classList.add('modal_visible');
+})
+
+exitBtn.addEventListener('click', function() {
+  loginModal.classList.remove('modal_visible');
+})

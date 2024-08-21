ed64b4905612ae0e14c32cbb05bc2d4c32ef96c8
Victor Lopez <vic.m.lopez114@gmail.com>
2024-08-21 13:52:34 -0400

    finished game, added a refresh button to restart the game without it reseting the high score unless page is refreshed

--- script.js
+++ script.js
@@ -16,29 +16,53 @@ let message = document.querySelector('#start_guessing');
 let gameScore = document.querySelector('#game_score');
 let gameHighScore = document.querySelector('#game_high-score');
 
-correctNumber.value = Math.trunc(Math.random() * 20) + 1;
-console.log(correctNumber.value);
+function startGame() {
+	correctNumber.value = Math.trunc(Math.random() * 20) + 1;
+	Number(gameScore.textContent);
 
-increaseNumberBtn.addEventListener('click', function () {
-	if (numberInput.value < 20) {
-		numberInput.value++;
-	}
-});
+	increaseNumberBtn.addEventListener('click', function () {
+		if (numberInput.value < 20) {
+			numberInput.value++;
+		}
+	});
 
-decreaseNumberBtn.addEventListener('click', function () {
-	if (numberInput.value > 1) {
-		numberInput.value--;
-	}
-});
+	decreaseNumberBtn.addEventListener('click', function () {
+		if (numberInput.value > 1) {
+			numberInput.value--;
+		}
+	});
 
-checkInputBtn.addEventListener('click', function () {
-	if (numberInput.value == correctNumber.value) {
-		message.textContent = 'Correct!';
-		gameBackground.style.backgroundColor = 'green';
-		correctNumber.textContent = `${correctNumber.value}`;
-	} else if (numberInput.value < correctNumber.value) {
-		message.textContent = 'Too Low!';
-	} else {
-		message.textContent = 'Too High!';
-	}
-});
+	checkInputBtn.addEventListener('click', function () {
+		if (numberInput.value == correctNumber.value) {
+			message.textContent = 'Correct!';
+			gameBackground.style.backgroundColor = 'green';
+			correctNumber.textContent = correctNumber.value;
+			gameHighScore.value = gameScore.textContent;
+
+			if (gameScore.textContent >= gameHighScore.textContent) {
+				gameHighScore.textContent = gameScore.textContent;
+			} else {
+				gameHighScore;
+			}
+		} else if (numberInput.value < correctNumber.value) {
+			message.textContent = 'Too Low!';
+			gameScore.textContent--;
+		} else {
+			message.textContent = 'Too High!';
+			gameScore.textContent--;
+		}
+	});
+
+	tryAgainBtn.addEventListener('click', function () {
+		correctNumber.value = Math.trunc(Math.random() * 20) + 1;
+		Number(gameScore.textContent);
+
+		correctNumber.textContent = '?';
+		message.textContent = 'Start Guessing...';
+		gameScore.textContent = '20';
+		gameBackground.style.backgroundColor = '#202020';
+		numberInput.value = '';
+	});
+}
+
+startGame();

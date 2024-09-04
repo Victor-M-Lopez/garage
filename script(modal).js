0637031d5c4e80b41e0503ac6dbe0209e8447319
Victor Lopez <vic.m.lopez114@gmail.com>
2024-09-04 17:02:40 -0400

    added code for modals to disappear and reappear

--- script(modal).js
+++ script(modal).js
@@ -4,19 +4,26 @@ const modalOverlay = document.querySelector('.overlay');
 const exitBtn = document.querySelector('#x-btn');
 const pageBody = document.querySelector('#modals');
 
+const openModal = function () {
+	modal1.classList.remove('hidden');
+	modalOverlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+	modal1.classList.add('hidden');
+	modalOverlay.classList.add('hidden');
+};
+
 for (let i = 0; i < openModalBtns.length; i++) {
-	openModalBtns[i].addEventListener('click', function () {
-		modal1.classList.remove('hidden');
-		modalOverlay.classList.remove('hidden');
-	});
+	openModalBtns[i].addEventListener('click', openModal);
 }
 
-exitBtn.addEventListener('click', function () {
-	modal1.classList.add('hidden');
-	modalOverlay.classList.add('hidden');
-});
+exitBtn.addEventListener('click', closeModal);
 
-modalOverlay.addEventListener('click', function () {
-	modal1.classList.add('hidden');
-	modalOverlay.classList.add('hidden');
+modalOverlay.addEventListener('click', closeModal);
+
+const escapeBtn = document.addEventListener('keydown', function (e) {
+	if (e.key === 'Escape' && !modal1.classList.contains('hidden')) {
+		closeModal();
+	}
 });

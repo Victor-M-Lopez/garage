46615ca469fa49dcdd6a8f8401fd230792e9d358
Victor Lopez <vic.m.lopez114@gmail.com>
2024-09-08 00:09:15 -0400

    dice game added hold and roll btn function just need to work on storing scores

--- script_dice.js
+++ script_dice.js
@@ -1,14 +1,61 @@
-// variables for player section for when it's their turn their side will light up
-const player_1 = document.querySelector('.player1');
-const player_2 = document.querySelector('.player2');
-
-// variables to store and display overall score and current score
-let playerScore = document.getElementById('player_score');
-let currentScore = document.getElementById('score');
-
-// buttons for rolling the dice, start a new game, or hold their current score
-const newGameBtn = document.getElementById('new_game-btn');
-const rollDiceBtn = document.getElementById('roll_dice-btn');
+// variables to target buttons
+const restartBtn = document.getElementById('restart-btn');
+const rollBtn = document.getElementById('roll-btn');
 const holdBtn = document.getElementById('hold-btn');
 
-let dice = document.getElementById('dice_img');
+// variables for targeting scores & variables for score values
+let activePlayer = 0;
+let currentScore = 0;
+
+let player0Score = document.getElementById('player0_score');
+let player1Score = document.getElementById('player1_score');
+
+let player0CurrentScore = document.getElementById('player0_current_score');
+let player1CurrentScore = document.getElementById('player1_current_score');
+
+let player1Side = document.getElementById('player1');
+let player2Side = document.getElementById('player2');
+
+// variables for dice img
+let diceImg = document.getElementById('dice_img');
+
+// when clicking on roll btn -> dice should appear, switch dice according to number, change numbers per click
+rollBtn.addEventListener('click', function () {
+	let dice = Math.trunc(Math.random() * 6) + 1;
+	console.log(dice);
+
+	diceImg.classList.remove('hidden');
+	diceImg.src = `assets/dice_${dice}.svg`;
+
+	// if number is 1 -> current score is 0
+	// otherwise the current score keeps adding up
+	if (dice !== 1) {
+		currentScore += dice;
+		document.getElementById(`player${activePlayer}_current_score`).textContent =
+			currentScore;
+	} else {
+		// switch player
+		currentScore = 0;
+		document.getElementById(
+			`player${activePlayer}_current_score`
+		).textContent = 0;
+		activePlayer = activePlayer === 0 ? 1 : 0;
+		player1Side.classList.toggle('active_player');
+		player2Side.classList.toggle('active_player');
+	}
+	console.log(currentScore);
+});
+
+// if player chooses to hold their current score
+// then add it to overall score and switch players
+holdBtn.addEventListener('click', function () {
+	document.getElementById(`player${activePlayer}_score`).textContent =
+		currentScore;
+	document.getElementById(
+		`player${activePlayer}_current_score`
+	).textContent = 0;
+	activePlayer = activePlayer === 0 ? 1 : 0;
+	player1Side.classList.toggle('active_player');
+	player2Side.classList.toggle('active_player');
+	currentScore = 0;
+});

44b0cbcd3ae1174f6b1fddea720140d151d542fe
Victor Lopez <vic.m.lopez114@gmail.com>
2024-09-11 00:17:29 -0400

    working on object and array destructuring

---
+++ script.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const houses = {
+	house1: {
+		location: '2202 redhood ln Rockville MD, 20091',
+		bedrooms: 2,
+		bathrooms: 1,
+		floors: 1,
+		backyard: 4.32,
+		img: {
+			room1: 'room1.jpeg',
+			room2: 'room2.jpeg',
+			bathroom: 'bathroom.jpeg',
+			backyard: 'backyard.jpeg',
+			frontyard: 'frontyard.jpeg',
+		},
+		price: [2320001, 1900],
+	},
+	house2: {
+		location: '1201 grey rd Rockville MD, 20091',
+		bedrooms: 4,
+		bathrooms: 2.5,
+		floors: 2,
+		backyard: 8.21,
+		img: {
+			room1: 'room1.jpeg',
+			room2: 'room2.jpeg',
+			bathroom: 'bathroom.jpeg',
+			backyard: 'backyard.jpeg',
+			frontyard: 'frontyard.jpeg',
+		},
+		price: [580201, 2100],
+	},
+};
+
+const apartments = {
+	apartment1: {
+		location: '3001 greenwood rd Bethesda MD, 22091',
+		bedrooms: 1,
+		bathrooms: 1,
+		floors: 1,
+		backyard: 0,
+		img: {
+			room1: 'room1.jpeg',
+			bathroom: 'bathroom.jpeg',
+			frontyard: 'frontyard.jpeg',
+			aminity: ['gym.jpeg', 'outdoor_playground.jpeg'],
+		},
+		price: [230210, 1820],
+	},
+
+	apartment2: {
+		location: '1021 harrison dr Bethesda MD, 12091',
+		bedrooms: 2,
+		bathrooms: 1.5,
+		floors: 1,
+		backyard: 0,
+		img: {
+			room1: 'room1.jpeg',
+			room2: 'room2.jpeg',
+			bathroom1: 'bathroom1.jpeg',
+			bathroom2: 'bathroom2.jpeg',
+			frontyard: 'frontyard.jpeg',
+			aminity: ['gym.jpeg', 'outdoor_playground.jpeg', 'pool.jpeg'],
+		},
+		price: [240210, 1920],
+	},
+};
